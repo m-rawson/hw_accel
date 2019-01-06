606177d4ffc2dec4ec029a818efed6a55b3878ee
Mason Rawson <mason@rawson.cc>
2019-01-05 16:32:29 -0800

    accounted for and added the prog support to the accel buffer mod

--- accel_buffer.sv
+++ accel_buffer.sv
@@ -1,6 +1,7 @@
 // accel buffer
 
 module accel_buffer #(
+  parameter int PROG_SUPPORT=1,
   parameter int PROG_FULL_N=5,
   parameter int PROG_EMPTY_N=5,
   parameter int BUFF_DEPTH=1024,
@@ -20,9 +21,14 @@ module accel_buffer #(
   Axis.Master to_mem
 );
 
+wire ib_full, ib_empty;
+wire ib_full_p, ib_empty_p;
+wire ob_full, ob_empty;
+wire ob_full_p, ob_empty_p;
 
 // from core mem, to accel
 fifo #(
+  .PROG_SUPPORT(PROG_SUPPORT),
   .PROG_EMPTY_N(PROG_EMPTY_N),
   .PROG_FULL_N(PROG_FULL_N),
   .BUFF_DEPTH(BUFF_DEPTH),
@@ -31,17 +37,18 @@ fifo #(
   .wr_clk(accel_clk),
   .wr_rst(rst),
   .wr_stream(from_mem),
-  .prog_full(input_buff_full),
-  .full(),
+  .prog_full(ib_full_p),
+  .full(ib_full),
   .rd_clk(mem_clk),
   .rd_rst(rst),
   .rd_stream(to_accel),
-  .prog_empty(input_buff_empty),
-  .empty()
+  .prog_empty(ib_empty_p),
+  .empty(ib_empty)
 );
 
 // from accel, to core mem
 fifo #(
+  .PROG_SUPPORT(PROG_SUPPORT),
   .PROG_EMPTY_N(PROG_EMPTY_N),
   .PROG_FULL_N(PROG_FULL_N),
   .BUFF_DEPTH(BUFF_DEPTH),
@@ -50,14 +57,19 @@ fifo #(
   .wr_clk(accel_clk),
   .wr_rst(rst),
   .wr_stream(from_accel),
-  .prog_full(output_buff_full),
-  .full(),
+  .prog_full(ob_full_p),
+  .full(ob_full),
   .rd_clk(mem_clk),
   .rd_rst(rst),
   .rd_stream(to_mem),
-  .prog_empty(output_buff_full),
-  .empty()
+  .prog_empty(ob_empty_p),
+  .empty(ob_empty)
 );
+
+assign output_buff_empty = (PROG_SUPPORT) ? ob_empty_p : ob_empty;
+assign output_buff_full  = (PROG_SUPPORT) ? ob_full_p  : ob_full;
+assign input_buff_empty  = (PROG_SUPPORT) ? ib_empty_p : ib_empty;
+assign input_buff_full   = (PROG_SUPPORT) ? ib_full_p  : ib_full;
 
 endmodule
 
